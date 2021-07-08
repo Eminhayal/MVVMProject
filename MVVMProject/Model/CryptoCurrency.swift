c143397dd7a90f4fc5a745c4ca4e46743d689c0c
Emin Hayal <eminhayal768@gmail.com>
2021-07-08 12:37:17 +0300

    MVVM Building

--- MVVMProject/Model/CryptoCurrency.swift
+++ MVVMProject/Model/CryptoCurrency.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct CryptoCurrency : Decodable {
+    
+    let currency : String
+    let price : String
+}
--- MVVMProject/Service/WebService.swift
+++ MVVMProject/Service/WebService.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class Webservice {
+    
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoCurrency]?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                
+                
+                let crytpoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                print(crytpoList)
+                
+                if let cryptoList = crytpoList {
+                    completion(crytpoList)
+                }
+                
+            }
+            
+        }.resume()
+        
+    }
+    
+}
+ 
